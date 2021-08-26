5d6812346f63fb0f122de499b713ea1d07225ae4
Julien Barrier <julien.barrier@manchester.ac.uk>
2021-08-26 10:02:35 +0100

    equation double gating

--- pyscripts/double_gate.py
+++ pyscripts/double_gate.py
@@ -25,3 +25,30 @@ class DoubleGate:
 
     def displacement(self):
         return (self.ctg * self.vtg - self.cbg * self.vbg)/2/epsilon_0
+
+
+class Gates:
+    def __init__(self,
+                n, D, Ctg:float, Cbg:float):
+        self.n = n
+        self.D = D
+        self.ctg = Ctg
+        self.cbg = Cbg
+
+    def top_gate(self):
+        return (e*self.n + 2*epsilon_0*self.D)/2/self.ctg
+
+    def back_gate(self):
+        return(e*self.n - 2*epsilon_0*self.D)/2/self.cbg
+
+    def equation(self):
+        coef = self.ctg/self.cbg
+        intercept = self.back_gate()[0] - self.top_gate()[0] * coef
+        print(f'V_b = {round(coef,3)} * V_t + {round(intercept,3)}')
+        return coef, intercept
+
+    def equation_tg(self):
+        coef = self.cbg/self.ctg
+        intercept = self.top_gate()[0] - self.back_gate()[0] * coef
+        print(f'V_b = {round(coef,3)} * V_t + {round(intercept,3)}')
+        return coef, intercept
